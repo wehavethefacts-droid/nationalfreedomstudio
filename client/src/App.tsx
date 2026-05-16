0ef770baafdd6447f3913bd1037b516cc7305c03
wehavethefacts-droid <wehavethefacts@gmail.com>
2026-05-16 04:41:50 +0000

    Add hash-based routing for GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Route, Switch, Router } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import ErrorBoundary from "./components/ErrorBoundary";
 import Layout from "./components/Layout";
 import MobileMenu from "./components/MobileMenu";
@@ -43,7 +44,9 @@ function App() {
       <ThemeProvider defaultTheme="light">
         <TooltipProvider>
           <Toaster />
-          <Router />
+          <Router hook={useHashLocation}>
+            <Router />
+          </Router>
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
